c6b69e4f386bede95ce62a686ffbbdb5dcc904e9
Taylor Edmiston <tedmiston@gmail.com>
2017-06-16 08:53:23 -0400

    Add word count

--- total_lines.sh
+++ total_lines.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Print out the total number of lines of Markdown written.
+# Print total line count and word count of all notes.
 
 line_count=$(find . -name '*.md' -print0 | xargs -0 wc -l | tail -n 1 | sed -e 's/total//' -e 's/ *//g')
+word_count=$(find . -name '*.md' -print0 | xargs -0 wc -w | tail -n 1 | sed -e 's/total//' -e 's/ *//g')
+
 echo $line_count "lines"
+echo $word_count "words"
